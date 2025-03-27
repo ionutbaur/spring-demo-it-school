2e3b87c1a4ef5d1437ac89fa23ba99d7480d2ec7
ionutbaur <ionut.baur@gmail.com>
2025-03-27 16:16:06 +0200

    added Swagger UI and some swagger doc

--- src/main/java/com/example/demo/controller/GreetingsController.java
+++ src/main/java/com/example/demo/controller/GreetingsController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.GreetingsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Greetings API", description = "This API just returns some greetings")
 @RestController
 public class GreetingsController {
 
@@ -14,6 +17,7 @@ public class GreetingsController {
         this.greetingsService = greetingsServiceImpl;
     }
 
+    @Operation(summary = "Get a greeting", description = "Returns Hello concatenated with the name parameter")
     @GetMapping("/greet/{name}")
     public String greet(@PathVariable String name) {
         return greetingsService.getGreeting(name);
